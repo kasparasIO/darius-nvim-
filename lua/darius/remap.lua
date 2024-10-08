6ed3daab1c3435dcd09d0e14277a84ecb4037fe4
kasparas <git@kasparas.io>
2024-10-08 20:23:24 +0300

    add one more remap

--- lua/darius/remap.lua
+++ lua/darius/remap.lua
@@ -8,3 +8,7 @@ vim.keymap.set("v", "<S-y>", '"+y', {desc = "Yank to plus register (clipboard)"}
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 
+-- move selected lines while holding alt
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
+
